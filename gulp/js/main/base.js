cbda0e3ccd11facc290f03e36087ca36e0004725
Antoine Vinial <antoine.vinial@gmail.com>
2015-10-26 21:25:04 +0100

    Remove oo librairy.

--- gulp/js/main/base.js
+++ gulp/js/main/base.js
@@ -1,15 +1,10 @@
 //import modules
-var oo = require('../libs/oo.js');
 var moduleExemple = require('./modules/module.js');
 
-(function ($, oo, win) {
+(function ($, win) {
 
     $(function () {
-
-        // oo.utils.placeholder();
-        // oo.utils.externalLinks();
         moduleExemple();
-
     });
 
 })(jQuery, oo, window);
